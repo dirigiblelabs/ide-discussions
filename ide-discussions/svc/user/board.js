1c86162e84b42dd7a8954e6cc86c658376f4cf13
delchev <delchevn@gmail.com>
2018-10-15 16:11:51 +0300

    close prevention

--- ide-discussions/svc/user/board.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-/*The board.js service for authenticated users*/
-require("ide-discussions/lib/boards_service_lib").create().execute();
--- ide-discussions/svc/user/comment.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-
-require("ide-discussions/lib/comments_service_lib").create().execute();
